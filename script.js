8f6b5b7aa9f65fc26ce97dad33050478761d42b1
Aditya Kumar <150278389+Sniper00001@users.noreply.github.com>
2024-12-09 16:33:49 +0530

    Update script.js
    update the link 'http://127.0.0.1:5000/predict'

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function predictPrice(carName, company, year, fuel_type) {
     try {
-        const response = await fetch('http://----------/predict', {
+        const response = await fetch('http://127.0.0.1:5000/predict', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
